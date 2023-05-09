9016b724046f4178ee8320fe9a225a96b1d7df77
lost <danielschoolxd@gmail.com>
2023-05-09 20:51:55 +0200

    please

--- Main.java
+++ Main.java
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
+import Windows.MainWindow;
+
 public class Main {
     public static void main(String[] args) {
         MainWindow mainWindow = new MainWindow();
         
     }
-=======
-import Windows.MainWindow;
-
-public class Main {
-    public static void main(String[] args) {
-        MainWindow mainWindow = new MainWindow();       
-    }
->>>>>>> bcc23a8 (added windows, music and exit png)
 }
